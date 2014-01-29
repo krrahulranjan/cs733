3b1bbb623eee72be13073c6e955b51ca9bd11352
krrahulranjan <100050038@iitb.ac.in>
2014-01-29 21:32:39 +0530

    Update cluster.go

--- cluster.go
+++ cluster.go
@@ -14,35 +14,15 @@ import (
 
 const (BROADCAST = -1)
 type Envelope struct {
-    // On the sender side, Pid identifies the receiving peer. If instead, Pid is
-    // set to cluster.BROADCAST, the message is sent to all peers. On the receiver side, the
-    // Id is always set to the original sender. If the Id is not found, the message is silently dropped
     Pid int
-   
-   
-    // An id that globally and uniquely identifies the message, meant for duplicate detection at
-    // higher levels. It is opaque to this package.
     MsgId int64
-   
-    // the actual message.
     Msg interface{}
 }
    
 type Server interface {
-    // Id of this server
     get_pid() int
-   
-    // array of other servers' ids in the same cluster
     get_peers() []int
-   
-   
-    // the channel to use to send messages to other peers
-    // Note that there are no guarantees of message delivery, and messages
-    // are silently dropped
     get_outbox() chan *Envelope
-   
-   
-    // the channel to receive messages from other peers.
     get_inbox() chan *Envelope
 }
 
